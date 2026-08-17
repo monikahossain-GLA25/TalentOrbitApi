598a0ab01e07a54b30f167c634286f19f73d7ae3
monika.hossain <monikahossain25@gmail.com>
2026-08-17 15:29:04 +0600

    Configure EF Core database context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TalentOrbitApi.Models;
+using TalentOrbitApi.Models.Entities;
+
+namespace TalentOrbitApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected ApplicationDbContext()
+        {
+        }
+
+        public DbSet<Mentor> Mentors { get; set; } = null!;
+    }
+}
