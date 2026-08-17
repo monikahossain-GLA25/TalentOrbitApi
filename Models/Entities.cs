dbf50501fc096da6929f421d37fd5f529bbeda5a
monika.hossain <monikahossain25@gmail.com>
2026-08-17 13:29:53 +0600

    Add project files.

---
+++ Models/Entities.cs
@@ -0,0 +1,15 @@
+﻿namespace TalentOrbitApi.Models.Entities
+{
+    public class Mentor
+    {
+        public Guid Id { get; set; }
+
+        public required string FullName { get; set; }
+
+        public required string EmailAddress { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public decimal HourlyRate { get; set; }
+    }
+}
