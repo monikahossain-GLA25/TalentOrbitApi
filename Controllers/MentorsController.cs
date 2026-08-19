bbded18ec0d7e1d03fc55e72924958b0a1d3032b
monika.hossain <monikahossain25@gmail.com>
2026-08-19 15:23:58 +0600

    complete the get part of the controller  GetAllMentors() and  GetAllMentorsById()

--- Controllers/MentorsController.cs
+++ Controllers/MentorsController.cs
@@ -13,6 +13,32 @@ namespace TalentOrbitApi.Controllers
         public MentorsController(ApplicationDbContext applicationDbContext) {
             this.applicationDbContext = applicationDbContext;
         }
-       
+
+
+        [HttpGet]
+        public IActionResult GetAllMentors()
+        {
+            var allMentors = applicationDbContext.Mentors.ToList();
+            return Ok(allMentors);
+        }
+
+        // GET: api/Mentors/{id}
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetMentorById(Guid id)
+        {
+            var mentor = applicationDbContext.Mentors.Find(id);
+            if (mentor == null)
+            {
+                return NotFound();
+
+            }
+            
+            return Ok(mentor);
+            
+        }
+
+   
+        
     }
 }
---
+++ Models/Dtos/UpdateMentorDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TalentOrbitApi.Models.Dtos
+{
+    public class UpdateMnentorDto
+    {
+        public string FullName { get; set; } = string.Empty;
+
+        public string EmailAddress { get; set; } = string.Empty;
+
+        public string? PhoneNumber { get; set; }
+
+        public decimal HourlyRate { get; set; }
+    }
+}
