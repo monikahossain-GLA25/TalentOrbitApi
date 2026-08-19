4b918e38e1c554f71ab449d36a3356b116812b95
monika.hossain <monikahossain25@gmail.com>
2026-08-19 12:23:40 +0600

    Add Mentors API Controller

---
+++ Controllers/MentorsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TalentOrbitApi.Data;
+
+namespace TalentOrbitApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MentorsController : ControllerBase
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public MentorsController(ApplicationDbContext applicationDbContext) {
+            this.applicationDbContext = applicationDbContext;
+        }
+       
+    }
+}
