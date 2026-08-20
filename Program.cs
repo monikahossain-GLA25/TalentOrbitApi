33a3d5ec7c3dfbefaedad8a1870740b177d92c86
monika.hossain <monikahossain25@gmail.com>
2026-08-20 13:45:31 +0600

    Configure mentor API launch settings

--- Program.cs
+++ Program.cs
@@ -3,14 +3,8 @@ using TalentOrbitApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
-
-// Register ApplicationDbContext BEFORE builder.Build()
 var connectionString = builder.Configuration
     .GetConnectionString("DefaultConnection")
     ?? throw new InvalidOperationException(
@@ -20,12 +14,6 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
