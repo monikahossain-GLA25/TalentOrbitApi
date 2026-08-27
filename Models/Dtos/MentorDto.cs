8b8c733bbc77f531f8dae158f911a9a43b217ef1
monika.hossain <monikahossain25@gmail.com>
2026-08-27 13:04:48 +0600

    Add mentor response and pagination DTOs

---
+++ Models/Dtos/MentorDto.cs
@@ -0,0 +1,15 @@
+﻿namespace TalentOrbitApi.Models.Dtos
+{
+    public class MentorDto
+    {
+        public Guid Id { get; set; }
+
+        public string FullName { get; set; } = string.Empty;
+
+        public string EmailAddress { get; set; } = string.Empty;
+
+        public string? PhoneNumber { get; set; }
+
+        public decimal HourlyRate { get; set; }
+    }
+}
