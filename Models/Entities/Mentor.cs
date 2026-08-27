b82e6730882afd70594a88fc50421080f7e2a201
monika.hossain <monikahossain25@gmail.com>
2026-08-27 09:39:19 +0600

    Add database constraints to Mentor

--- Models/Entities/Mentor.cs
+++ Models/Entities/Mentor.cs
@@ -1,15 +1,22 @@
-﻿namespace TalentOrbitApi.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace TalentOrbitApi.Models.Entities
 {
     public class Mentor
     {
         public Guid Id { get; set; }
 
+        [MaxLength(100)]
         public required string FullName { get; set; }
 
+        [MaxLength(150)]
         public required string EmailAddress { get; set; }
 
+        [MaxLength(20)]
         public string? PhoneNumber { get; set; }
 
+        [Precision(18, 2)]
         public decimal HourlyRate { get; set; }
     }
 }
